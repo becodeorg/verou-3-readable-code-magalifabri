9f7ce3b382ecfe23249c6e62fd10b2d4ffd1075c
Magali Fabri <magali_90@live.nl>
2022-02-21 10:18:25 +0100

    Refactor: move data arrays to seperate file

---
+++ data.php
@@ -0,0 +1,31 @@
+<?php
+
+$customers = [
+    "koen" => [
+        "name" => "koen",
+        "address" => "a yacht in Antwerp",
+    ],
+    "manuele" => [
+        "name" => "manuele",
+        "address" => "somewhere in Belgium",
+    ],
+    "students" => [
+        "name" => "students",
+        "address" => "BeCode office",
+    ],
+];
+
+$pizzas = [
+    "marguerita" => [
+        "name" => "marguerita",
+        "price" => 5,
+    ],
+    "golden" => [
+        "name" => "golden",
+        "price" => 100,
+    ],
+    "calzone" => [
+        "name" => "calzone",
+        "price" => 10,
+    ],
+];
--- experimental.php
+++ experimental.php
@@ -1,34 +1,7 @@
 <?php
 
-$customers = [
-    "koen" => [
-        "name" => "koen",
-        "address" => "a yacht in Antwerp",
-    ],
-    "manuele" => [
-        "name" => "manuele",
-        "address" => "somewhere in Belgium",
-    ],
-    "students" => [
-        "name" => "students",
-        "address" => "BeCode office",
-    ],
-];
 
-$pizzas = [
-    "marguerita" => [
-        "name" => "marguerita",
-        "price" => 5,
-    ],
-    "golden" => [
-        "name" => "golden",
-        "price" => 100,
-    ],
-    "calzone" => [
-        "name" => "calzone",
-        "price" => 10,
-    ],
-];
+require "./data.php";
 
 
 function placeOrder($pizza, $customer)
